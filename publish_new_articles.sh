06b88884cedc83466886402e01db6d7f0322452b
dailybrew <87592604+dailybrew@users.noreply.github.com>
2021-07-18 22:52:38 +0200

    Update publish_new_articles.sh

--- publish_new_articles.sh
+++ publish_new_articles.sh
@@ -8,16 +8,16 @@ PUBLISHED=$BLOG_PATH/source/_posts
 cd $PENDING
 if [ "$(ls -A $1)" ]; then
 	## REMOVE SPACES
-    for post in *; do mv "$post" `echo $post | tr ' ' '_'` ; done
-    ## LIST OF NEW ARTICLES
-	new_articles=$(ls ${PENDING})
-	## PUBLISH ARTICLES BY MOVING THEM TO THE SOURCE FOLDER
-	for post in $new_articles
-	do
-		## -- here -- > 
-		cd $BLOG_PATH
-		hexo new $post && mv $PENDING/$post $PUBLISHED/$post
-	done
+	for post in *; do mv "$post" `echo $post | tr ' ' '_'` ; done
+		## LIST OF NEW ARTICLES
+		new_articles=$(ls ${PENDING})
+		## PUBLISH ARTICLES BY MOVING THEM TO THE SOURCE FOLDER
+		for post in $new_articles
+		do
+			## -- here -- > 
+			cd $BLOG_PATH
+			hexo new $post && mv $PENDING/$post $PUBLISHED/$post
+		done
 else 
     echo "Nothing to publish"
 fi
